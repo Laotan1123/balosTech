157124d743bf79f7335cfecc28ceef111f390699
Laotan1123 <Laotan2300@gmail.com>
2025-03-31 00:57:53 +0100

    Fix tsconfig.node.json encoding and format

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/provider'
 import { CartProvider } from './context/CartContext'
 import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ChakraProvider>
         <AuthProvider>
           <CartProvider>
@@ -17,6 +17,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           </CartProvider>
         </AuthProvider>
       </ChakraProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
